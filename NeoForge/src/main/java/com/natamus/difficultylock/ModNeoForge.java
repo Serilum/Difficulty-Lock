74b5722edaa71844daa101b7a0b042b769edb7d5
Rick South <rick@natamus.com>
2023-12-12 10:30:41 +0100

    Difficulty Lock source code for 1.20.3.

---
+++ NeoForge/src/main/java/com/natamus/difficultylock/ModNeoForge.java
@@ -0,0 +1,36 @@
+package com.natamus.difficultylock;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.difficultylock.neoforge.config.IntegrateNeoForgeConfig;
+import com.natamus.difficultylock.neoforge.events.NeoForgeDifficultyLockEvent;
+import com.natamus.difficultylock.util.Reference;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateNeoForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		NeoForge.EVENT_BUS.register(NeoForgeDifficultyLockEvent.class);
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
---
+++ NeoForge/src/main/java/com/natamus/difficultylock/neoforge/events/NeoForgeDifficultyLockEvent.java
@@ -0,0 +1,22 @@
+package com.natamus.difficultylock.neoforge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.difficultylock.events.DifficultyLockEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.level.LevelEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeDifficultyLockEvent {
+	@SubscribeEvent
+	public static void onWorldLoad(LevelEvent.Load e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		DifficultyLockEvent.onWorldLoad((ServerLevel)level);
+	}
+}
