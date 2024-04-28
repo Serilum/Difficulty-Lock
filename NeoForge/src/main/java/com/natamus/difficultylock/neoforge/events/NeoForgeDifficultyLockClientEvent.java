8abe25d69483032fe856df0b2b29069920bbdcbf
Rick South <rick@natamus.com>
2024-04-28 19:02:52 +0200

    (v4.2) (1.20.5 Neo) Updated to work with NeoForge's TickEvent changes in 20.5.14.

--- NeoForge/src/main/java/com/natamus/difficultylock/neoforge/events/NeoForgeDifficultyLockClientEvent.java
+++ NeoForge/src/main/java/com/natamus/difficultylock/neoforge/events/NeoForgeDifficultyLockClientEvent.java
@@ -5,16 +5,12 @@ import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.event.TickEvent;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
 
 @EventBusSubscriber(value = Dist.CLIENT)
 public class NeoForgeDifficultyLockClientEvent {
 	@SubscribeEvent
-	public void onClientTick(TickEvent.ClientTickEvent e) {
-		if (!e.phase.equals(TickEvent.Phase.END)) {
-			return;
-		}
-
+	public void onClientTick(ClientTickEvent.Post e) {
 		DifficultyLockEvent.onClientTick(Minecraft.getInstance());
 	}
 }
