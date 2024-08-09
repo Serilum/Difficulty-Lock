ebc6d5338da41ef529b09e44c5f4d3fa876ce9df
Rick South <rick@natamus.com>
2024-08-09 08:27:18 +0200

    Difficulty Lock source code for 1.21.1.

---
+++ Common/src/main/java/com/natamus/difficultylock/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.difficultylock.util;
+
+public class Reference {
+	public static final String MOD_ID = "difficultylock";
+	public static final String NAME = "Difficulty Lock";
+	public static final String VERSION = "4.6";
+	public static final String ACCEPTED_VERSIONS = "[1.21.1]";
+}
---
+++ Fabric/src/main/java/com/natamus/difficultylock/ModFabric.java
@@ -0,0 +1,37 @@
+package com.natamus.difficultylock;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.difficultylock.events.DifficultyLockEvent;
+import com.natamus.difficultylock.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		ServerWorldEvents.LOAD.register((MinecraftServer server, ServerLevel world) -> {
+			DifficultyLockEvent.onWorldLoad(world);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
