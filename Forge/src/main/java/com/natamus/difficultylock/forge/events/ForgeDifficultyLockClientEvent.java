98f7f058a454e2c5ad7b1f13c4f515bc90f1b4d1
Rick South <rick@natamus.com>
2025-02-12 23:37:48 +0100

    (v4.7) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/difficultylock/forge/events/ForgeDifficultyLockClientEvent.java
+++ Forge/src/main/java/com/natamus/difficultylock/forge/events/ForgeDifficultyLockClientEvent.java
@@ -5,12 +5,10 @@ import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber(value = Dist.CLIENT)
 public class ForgeDifficultyLockClientEvent {
 	@SubscribeEvent
-	public void onClientTick(TickEvent.ClientTickEvent e) {
+	public static void onClientTick(TickEvent.ClientTickEvent e) {
 		if (!e.phase.equals(TickEvent.Phase.END)) {
 			return;
 		}
--- NeoForge/src/main/java/com/natamus/difficultylock/neoforge/events/NeoForgeDifficultyLockEvent.java
+++ NeoForge/src/main/java/com/natamus/difficultylock/neoforge/events/NeoForgeDifficultyLockEvent.java
@@ -5,10 +5,8 @@ import com.natamus.difficultylock.events.DifficultyLockEvent;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.level.LevelEvent;
 
-@EventBusSubscriber
 public class NeoForgeDifficultyLockEvent {
 	@SubscribeEvent
 	public static void onWorldLoad(LevelEvent.Load e) {
