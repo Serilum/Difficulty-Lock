c8fc16f59e650763ffda39216b151ad9b1e9134b
Rick South <rick@natamus.com>
2025-10-03 22:37:33 +0200

    Difficulty Lock source code for 1.21.9.

---
+++ NeoForge/src/main/java/com/natamus/difficultylock/neoforge/events/NeoForgeDifficultyLockEvent.java
@@ -0,0 +1,20 @@
+package com.natamus.difficultylock.neoforge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.difficultylock.events.DifficultyLockEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.level.LevelEvent;
+
+public class NeoForgeDifficultyLockEvent {
+	@SubscribeEvent
+	public static void onWorldLoad(LevelEvent.Load e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		DifficultyLockEvent.onWorldLoad((ServerLevel)level);
+	}
+}
